317494ae06cc49f186ba9599c25f531fbc630440
Benji Schussheim <bas101@duke.edu>
2023-03-25 14:47:22 -0400

    made screens for comp tab nav

--- src/screens/competitions/CompetitionOverviewScreen.js
+++ src/screens/competitions/CompetitionOverviewScreen.js
@@ -3,22 +3,31 @@ import { TouchableOpacity } from "react-native"
 import { Box, Text, Button, HStack, VStack, Icon } from "native-base"
 import { useNavigation } from "@react-navigation/native"
 import { Ionicons, MaterialCommunityIcons } from "@expo/vector-icons"
+import { CompetitionContext } from "../../context/CompetitionContext"
+import { DisplayContext } from "../../context/DisplayContext"
 
 function CompetitionOverviewScreen() {
 	const navigation = useNavigation()
+	const { competitionsMap } = useContext(CompetitionContext)
+	const { currentCompetitionId } = useContext(DisplayContext)
+	const competitionData = competitionsMap[currentCompetitionId]
+	if (!competitionData) return null
+	const {
+		_id,
+		organizerId,
+		coverImage,
+		title,
+		subtitle,
+		overview,
+		prizeMoney,
+		deadline,
+		rules,
+		resources,
+		createdAt,
+	} = competitionData
 	return (
 		<Box variant="screen" mt="33">
-			<Text>CompetitionOverviewScreen</Text>
-			<Button
-				onPress={() => {
-					console.log("navigating")
-					navigation.navigate("competitions", {
-						screen: "competitionsScreen",
-					})
-				}}
-			>
-				<Text>Navigate back to commpetitions screen</Text>
-			</Button>
+			<Text>{overview}</Text>
 		</Box>
 	)
 }
--- src/screens/competitions/CompetitionResourcesScreen.js
+++ src/screens/competitions/CompetitionResourcesScreen.js
@@ -3,12 +3,31 @@ import { TouchableOpacity } from "react-native"
 import { Box, Text, Button, HStack, VStack, Icon } from "native-base"
 import { useNavigation } from "@react-navigation/native"
 import { Ionicons, MaterialCommunityIcons } from "@expo/vector-icons"
+import { CompetitionContext } from "../../context/CompetitionContext"
+import { DisplayContext } from "../../context/DisplayContext"
 
 function CompetitionResourcesScreen() {
 	const navigation = useNavigation()
+	const { competitionsMap } = useContext(CompetitionContext)
+	const { currentCompetitionId } = useContext(DisplayContext)
+	const competitionData = competitionsMap[currentCompetitionId]
+	if (!competitionData) return null
+	const {
+		_id,
+		organizerId,
+		coverImage,
+		title,
+		subtitle,
+		overview,
+		prizeMoney,
+		deadline,
+		rules,
+		resources,
+		createdAt,
+	} = competitionData
 	return (
-		<Box variant="screen" mt="12">
-			<Text>CompetitionResourcesScreen</Text>
+		<Box variant="screen" mt="33">
+			<Text>{resources}</Text>
 		</Box>
 	)
 }
