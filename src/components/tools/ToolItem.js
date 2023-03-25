43e09b2685435e5030260c4f8fd5928fdd980f05
Benji Schussheim <bas101@duke.edu>
2023-03-25 18:38:57 -0400

    changing tools screen

--- src/components/tools/ToolItem.js
+++ src/components/tools/ToolItem.js
@@ -1,44 +1,44 @@
 import { useState, useEffect, useContext } from "react"
 import { TouchableOpacity, Linking } from "react-native"
 import {
-    Box,
-    Text,
-    Button,
-    HStack,
-    VStack,
-    Icon,
-    Pressable,
-    Image,
-    Heading,
+	Box,
+	Text,
+	Button,
+	HStack,
+	VStack,
+	Icon,
+	Pressable,
+	Image,
+	Heading,
 } from "native-base"
 import { useNavigation } from "@react-navigation/native"
 import { Ionicons, MaterialCommunityIcons } from "@expo/vector-icons"
 import { formatDate } from "../../util/date/formatDate"
 
 function ToolItem({ toolId }) {
-    const navigation = useNavigation()
-    //const { title, subtitle } = toolData
-    return (
-        <Pressable
-            onPress={Linking.openURL("https://www.example.com")}
-            _hover={{ bg: "c1.800" }}
-            p="2"
-            borderRadius="10"
-        >
-            <HStack justifyContent="space-between" mx="4">
-                <HStack space="3">
-                    <VStack>
-                        <Text fontSize="16px" maxW="400px">
-                            {"title"}
-                        </Text>
-                        <Text fontSize="13px" maxW="400px" opacity={0.7}>
-                            {"subtitle"}
-                        </Text>
-                    </VStack>
-                </HStack>
-            </HStack>
-        </Pressable>
-    )
+	const navigation = useNavigation()
+	//const { title, subtitle } = toolData
+	return (
+		<Pressable
+			// onPress={Linking.openURL("https://www.example.com")}
+			_hover={{ bg: "c1.800" }}
+			p="2"
+			borderRadius="10"
+		>
+			<HStack justifyContent="space-between" mx="4">
+				<HStack space="3">
+					<VStack>
+						<Text fontSize="16px" maxW="400px">
+							{"title"}
+						</Text>
+						<Text fontSize="13px" maxW="400px" opacity={0.7}>
+							{"subtitle"}
+						</Text>
+					</VStack>
+				</HStack>
+			</HStack>
+		</Pressable>
+	)
 }
 
 export { ToolItem }
--- src/screens/tools/ToolsScreen.js
+++ src/screens/tools/ToolsScreen.js
@@ -6,35 +6,35 @@ import { Ionicons, MaterialCommunityIcons } from "@expo/vector-icons"
 import { ToolItem } from "../../components/tools/ToolItem"
 
 function ToolsScreen() {
-    const navigation = useNavigation()
-    const Data = [
-        {
-            title: "Tool 1",
-            subtitle: "This is tool 1",
-        },
-        {
-            title: "Tool 2",
-            subtitle: "This is a description of tool 2",
-        },
-    ]
+	const navigation = useNavigation()
+	const DATA = [
+		{
+			title: "Tool 1",
+			subtitle: "This is tool 1",
+		},
+		{
+			title: "Tool 2",
+			subtitle: "This is a description of tool 2",
+		},
+	]
 
-    return (
-        <Box variant="screen">
-            <HStack justifyContent="space-between" alignItmes="center">
-                <VStack space="1" maxWidth="500px">
-                    <Heading fontSize="30px">Tools</Heading>
-                    <Heading fontSize="14px">
-                        Here are some models to kick off your gAI adventure!
-                    </Heading>
-                </VStack>
-            </HStack>
-            <VStack space="1" mt="5">
-                {Data.map((toolId, index) => (
-                    <ToolItem key={index.toString()} toolId={toolId} />
-                ))}
-            </VStack>
-        </Box>
-    )
+	return (
+		<Box variant="screen">
+			<HStack justifyContent="space-between" alignItmes="center">
+				<VStack space="1" maxWidth="500px">
+					<Heading fontSize="30px">Tools</Heading>
+					<Heading fontSize="14px">
+						Here are some models to kick off your gAI adventure!
+					</Heading>
+				</VStack>
+			</HStack>
+			<VStack space="1" mt="5">
+				{DATA.map((toolId, index) => (
+					<ToolItem key={index.toString()} toolId={toolId} />
+				))}
+			</VStack>
+		</Box>
+	)
 }
 
 export { ToolsScreen }
