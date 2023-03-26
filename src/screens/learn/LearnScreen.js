88f2364cdc93022644bc21494cefa9b1022c1449
Ashrit Verma <ashrit.verma@duke.edu>
2023-03-25 21:09:35 -0400

    fixing white under problem

--- src/screens/learn/LearnScreen.js
+++ src/screens/learn/LearnScreen.js
@@ -6,42 +6,46 @@ import { Ionicons, MaterialCommunityIcons } from "@expo/vector-icons"
 import { ToolItem } from "../../components/tools/ToolItem"
 
 function LearnScreen() {
-	const navigation = useNavigation()
-	const DATA = [
-		{
-			title: "Article 1",
-			subtitle: "This is article 1",
-			coverPhoto: "https://picsum.photos/id/103/200/200",
-			url: "https://www.google.com",
-			date: new Date("2021-01-01"),
-		},
-		{
-			title: "Article 2",
-			subtitle: "This is a description of article 2",
-			coverPhoto: "https://picsum.photos/id/104/200/200",
-			url: "https://www.nytimes.com",
-			date: new Date("2021-01-02"),
-		},
-	]
+    const navigation = useNavigation()
+    const DATA = [
+        {
+            title: "Article 1",
+            subtitle: "This is article 1",
+            coverPhoto: "https://picsum.photos/id/103/200/200",
+            url: "https://www.google.com",
+            date: new Date("2021-01-01"),
+        },
+        {
+            title: "Article 2",
+            subtitle: "This is a description of article 2",
+            coverPhoto: "https://picsum.photos/id/104/200/200",
+            url: "https://www.nytimes.com",
+            date: new Date("2021-01-02"),
+        },
+    ]
 
-	return (
-		<Box variant="screen">
-			<HStack justifyContent="space-between" alignItmes="center">
-				<VStack space="1" maxWidth="500px">
-					<Heading fontSize="30px">Learn</Heading>
-					<Heading fontSize="14px">
-						Stay up to date on all things gAI and build background knowledge to
-						win competitions!
-					</Heading>
-				</VStack>
-			</HStack>
-			<VStack space="1" mt="4">
-				{DATA.map((toolData, index) => (
-					<ToolItem key={index.toString()} toolData={toolData} showDate />
-				))}
-			</VStack>
-		</Box>
-	)
+    return (
+        <Box variant="screen">
+            <HStack justifyContent="space-between" alignItmes="center">
+                <VStack space="1" maxWidth="500px">
+                    <Heading fontSize="30px">Learn</Heading>
+                    <Heading fontSize="14px">
+                        Stay up to date on all things gAI and build background
+                        knowledge to win competitions!
+                    </Heading>
+                </VStack>
+            </HStack>
+            <VStack space="1" mt="4">
+                {DATA.map((toolData, index) => (
+                    <ToolItem
+                        key={index.toString()}
+                        toolData={toolData}
+                        showDate
+                    />
+                ))}
+            </VStack>
+        </Box>
+    )
 }
 
 export { LearnScreen }
